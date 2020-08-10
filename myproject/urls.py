cbfbc82b28d0aa324c370bdbb28139fa8bde4c71
dell <maqsad.novruzov@gmail.com>
2020-08-10 07:17:43 +0400

    init

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,4 +1,4 @@
-"""DjangoTodoApp URL Configuration
+"""django_todo_app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DjangoTodoApp project.
+WSGI config for django_todo_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoTodoApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_todo_app.settings')
 
 application = get_wsgi_application()
